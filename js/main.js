2ec779f486d7df572ad98f68c874247ff2f3e637
moonFlame3 <windkage29@gmail.com>
2018-05-04 17:33:48 +0630

    Add feature testing for video testing. Add js for js code. Change 'css.txt' to 'overviews,txt' .

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var home_sec = document.getElementById("home_sec");
+
+function hide_W_time() {
+    home_sec.style.display = "none";
+}
+
+function main() {
+    setTimeout(hide_W_time, 1400);
+}
+
+main();
